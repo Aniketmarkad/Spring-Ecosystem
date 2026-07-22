d133209391998d6e67573088601461fbd10161c6
Aniket Markad <aniket45.markad@gmail.com>
2026-07-22 14:28:00 +0530

    Update Repository.java

--- 001_SpringCore/03SpringCoreXmlRefTypeEx/src/main/java/com/aniket/Repository.java
+++ 001_SpringCore/03SpringCoreXmlRefTypeEx/src/main/java/com/aniket/Repository.java
@@ -7,19 +7,17 @@ public class Repository {
 	public Repository() {
 		super();
 	}
-	//alt+shift+S  > R  > Select All > Generate
+	
 	public String getEntity() {
 		return entity;
 	}
 	public void setEntity(String entity) {
 		this.entity = entity;
 	}
-	//alt+shift+S  > S  > Generate
+	
 	@Override
 	public String toString() {
 		return "Repository [entity=" + entity + "]";
 	}
-	
-	
-	
+		
 }
