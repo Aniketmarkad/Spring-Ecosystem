7648310f38ad0379e1594c7750533441da1ed758
anii <aniket45.markad@gmail.com>
2026-07-24 18:00:15 +0530

    18 SpringCore CircularDependency  example added

---
+++ 001_SpringCore/18SpringCoreCirDep/src/main/java/com/aniket/Employee.java
@@ -0,0 +1,37 @@
+package com.aniket;
+
+public class Employee {
+
+	private String name;
+	
+	private Project pob;
+
+	public Employee() {
+		super();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+		public Project getPob() {
+		return pob;
+	}
+
+	public void setPob(Project pob) {
+		this.pob = pob;
+		System.out.println("EMPLOYEE.setProject ");
+
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", pob=" + pob + "]";
+	}
+	
+	
+}
