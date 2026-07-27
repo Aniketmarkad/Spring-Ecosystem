98f0db563a9e9f110cd5de66aa9e4b3183a8d905
anii <aniket45.markad@gmail.com>
2026-07-27 18:21:46 +0530

    01SpringBootCore RunnerEx Added

---
+++ 002-SpringBootCore/01SpringBootCoreRunnersEx/src/main/java/com/anii/DatabaseConRunner.java
@@ -0,0 +1,17 @@
+package com.anii;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(-10)
+public class DatabaseConRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println("DatabaseCon Config");
+	}
+
+}
---
+++ 002-SpringBootCore/01SpringBootCoreRunnersEx/src/main/java/com/anii/EmailConfigRunner.java
@@ -0,0 +1,18 @@
+package com.anii;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(0)
+public class EmailConfigRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println("EMAIL CONFIG");
+
+	}
+
+}
