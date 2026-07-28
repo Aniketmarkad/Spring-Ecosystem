04b18d4ab11e1346ebe9d72407cfd6d8dece1d94
anii <aniket45.markad@gmail.com>
2026-07-28 20:02:35 +0530

    04SpringBootConfigProperties ex2 added

---
+++ 002-SpringBootCore/04SpringBootConfigPropertiesEx2/src/main/java/com/anii/Process.java
@@ -0,0 +1,25 @@
+package com.anii;
+
+public class Process {
+
+	private Integer pid;
+	private String pcode;
+	public Integer getPid() {
+		return pid;
+	}
+	public void setPid(Integer pid) {
+		this.pid = pid;
+	}
+	public String getPcode() {
+		return pcode;
+	}
+	public void setPcode(String pcode) {
+		this.pcode = pcode;
+	}
+	@Override
+	public String toString() {
+		return "Process [pid=" + pid + ", pcode=" + pcode + "]";
+	}
+	
+	
+}
