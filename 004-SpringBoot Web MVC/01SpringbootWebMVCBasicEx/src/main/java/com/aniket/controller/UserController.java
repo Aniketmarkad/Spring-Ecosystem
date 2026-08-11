368b66e01330759211f75954de78539fff44ce27
anii <aniket45.markad@gmail.com>
2026-08-11 12:57:43 +0530

    01SpringBootMVC first ex added

---
+++ 004-SpringBoot Web MVC/01SpringbootWebMVCBasicEx/src/main/java/com/aniket/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.aniket.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+	@GetMapping("/home")
+	public String showHomePage()
+	{
+		return "UserHome";
+	}
+}
+  
---
+++ 004-SpringBoot Web MVC/01SpringbootWebMVCBasicEx/src/main/webapp/WEB-INF/pages/UserHome.jsp
@@ -0,0 +1,8 @@
+<html>
+	<head>
+		<title> WELCOME TO APP</title>
+	</head>
+	<body>
+		<h2>Welcome To First Application..!</h2>
+	</body>
+</html>
