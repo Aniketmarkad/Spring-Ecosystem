778b6e1e8808a696bff4a81a1027946cc66d33a9
anii <aniket45.markad@gmail.com>
2026-09-09 14:39:29 +0530

    08 Springboot REST crud with swggwer an openapi ex

---
+++ 005-SpringBoot REST/08SpringBootRestCrudMySql/src/main/java/com/aniket/Application.java
@@ -0,0 +1,20 @@
+package com.aniket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
+@SpringBootApplication
+@OpenAPIDefinition
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+} 
