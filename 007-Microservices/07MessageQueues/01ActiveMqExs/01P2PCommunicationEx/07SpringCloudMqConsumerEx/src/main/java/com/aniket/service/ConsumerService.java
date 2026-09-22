22944c7234e58225e3929069f4213df99ae11a3d
anii <aniket45.markad@gmail.com>
2026-09-22 16:50:18 +0530

    07 Message Queue ActiveMq P2P Communication ex added

---
+++ 007-Microservices/07MessageQueues/01ActiveMqExs/01P2PCommunicationEx/07SpringCloudMqConsumerEx/src/main/java/com/aniket/service/ConsumerService.java
@@ -0,0 +1,15 @@
+package com.aniket.service;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConsumerService {
+
+	@JmsListener(destination = "${my.app.desti-name}")
+	public void readMsg(String message)
+	{
+		System.out.println(message); 
+	}
+}
+      
