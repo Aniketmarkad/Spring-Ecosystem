bc8d9404ecfbe28f6f2c823599a8b99a6a74dcd7
anii <aniket45.markad@gmail.com>
2026-09-23 18:53:13 +0530

    08 api gateway cart service ex added

---
+++ 007-Microservices/08ApiGatewayEx/03SpringCloudCartServiceFeignClientEx/src/main/java/com/aniket/rest/CartRestController.java
@@ -0,0 +1,51 @@
+package com.aniket.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.aniket.entity.Cart;
+
+@RestController
+@RequestMapping("/cart")
+@RefreshScope
+public class CartRestController {
+
+	@Value("${my.app.title}")
+	private String title;
+
+	@GetMapping("/info")
+	public ResponseEntity<String> showMessage(
+			//@RequestHeader("MyToken") String token 
+			)
+	{
+		return ResponseEntity.ok("WELCOME TO CART SERVICE =>"  +title);
+	}
+	
+	@GetMapping("/find/{id}")
+	public ResponseEntity<Cart> getCartById(@PathVariable("id")Integer id) {
+		Cart cart = new Cart();
+		cart.setCartId(id);
+		cart.setCartCost(2300.0);
+		cart.setCartCode("TEST");
+		return ResponseEntity.ok(cart);
+	}
+	
+	@PostMapping("/create")
+	public ResponseEntity<String> addToCart(@RequestBody Cart cart) {
+		return ResponseEntity.ok("ADDED TO CART => " + cart);
+	}
+	@GetMapping("/test")
+	public ResponseEntity<String> testCart() {
+
+	    return ResponseEntity.ok("CART-SERVICE reached");
+	}
+
+}
