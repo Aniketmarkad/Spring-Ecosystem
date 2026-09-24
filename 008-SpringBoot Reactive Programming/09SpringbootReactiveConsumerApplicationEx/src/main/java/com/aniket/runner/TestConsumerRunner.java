2ad2ff0034ce1bb237d98114a791e01e72ada3c9
anii <aniket45.markad@gmail.com>
2026-09-24 11:19:16 +0530

    008 Spring Boot Reactive Programming Consumer ex added

---
+++ 008-SpringBoot Reactive Programming/09SpringbootReactiveConsumerApplicationEx/src/main/java/com/aniket/runner/TestConsumerRunner.java
@@ -0,0 +1,49 @@
+package com.aniket.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.aniket.entity.Student;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class TestConsumerRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		//1. Create WebClient object using base URL 
+				WebClient client = WebClient.create("http://localhost:9090/student");
+				
+				//call 
+				/*
+				Mono<Student> result = 
+						client
+						.post() //Http Method
+						.uri("/create") //PATH
+						.body(Mono.just(new Student("AA256", "SAM", 300.0)), Student.class) //Body
+						.retrieve() // execute
+						.bodyToMono(Student.class); //convert response
+				
+				System.out.println("FROM CONSUMER ---->");
+				//access result
+				result.subscribe(System.out::println);
+				*/
+				
+				
+				/*
+				Flux<Student> flux =  client.get().uri("/all").retrieve().bodyToFlux(Student.class);
+				flux.doOnNext(System.out::println).blockLast();
+				*/
+				
+				Mono<Student> result = 
+				client.get().uri("/fetch/AA256").retrieve() 
+				.bodyToMono(Student.class);
+				result.subscribe(System.out::println);
+			}
+
+	}
+
+
